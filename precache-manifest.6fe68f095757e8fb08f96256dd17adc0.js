5570a24ab35f951f10b113d9fefb2ac9a2cc8dd7
Daniyal <Daniyal3siddiqui@gmail.com>
2020-06-29 23:21:49 +0500

    Updates

---
+++ precache-manifest.6fe68f095757e8fb08f96256dd17adc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc79b7c78973963e1743a5eb0d7ec23",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "0b68941a6595ab6f7eef",
+    "url": "/Weather/static/css/main.63401e7a.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/Weather/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "0b68941a6595ab6f7eef",
+    "url": "/Weather/static/js/main.f640ab6e.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "a9c7531ebcf900503f03062e2a59d719",
+    "url": "/Weather/static/media/2.a9c7531e.jpg"
+  }
+]);
